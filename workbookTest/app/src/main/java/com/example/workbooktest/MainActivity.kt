6871dd2b80130f6d84c8f560e14d6ce5cbc78404
udomai <ekfonitis@gmail.com>
2024-12-27 11:43:27 +0100

    upload the first local training project

---
+++ workbookTest/app/src/main/java/com/example/workbooktest/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.workbooktest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.workbooktest.ui.theme.WorkbookTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WorkbookTestTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) { Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WorkbookTestTheme {
+        Greeting("Android")
+    }
+}
---
+++ workbookTest/readme.md
@@ -0,0 +1,4 @@
+Collection of android projects
+
+- practice projects
+- maybe even the first serious ones
